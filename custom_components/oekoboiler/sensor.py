30c7f5161f0c4b98b3bd99df9f2d375e5ce75625
Sebastain Plattner <sebastian.plattner@gmail.com>
2022-02-01 19:11:57 +0100

    add dummy water and set temp sensor

--- custom_components/oekoboiler/sensor.py
+++ custom_components/oekoboiler/sensor.py
@@ -4,6 +4,10 @@ from typing import Callable, Union
 
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import (
+    TEMP_CELSIUS, 
+    DEVICE_CLASS_TEMPERATURE,
+)
 
 from homeassistant.helpers.typing import ConfigType, HomeAssistantType
 
@@ -28,7 +32,21 @@ async def async_setup_entry(
 
     devices: dict = []
 
+    # Mode Sensor
     devices.append(OekoboilerModeSensorEntiry(
+                hass=hass,
+            )
+    )
+
+    # Water Temp Sensor
+    devices.append(OekoboilerWaterTempSensorEntiry(
+                hass=hass,
+            )
+    )
+
+
+    # Set Temp Sensor
+    devices.append(OekoboilerSetTempSensorEntiry(
                 hass=hass,
             )
     )
@@ -66,6 +84,117 @@ class OekoboilerModeSensorEntiry(SensorEntity):
         return self._state
 
 
+
+    @property
+    def device_info(self) -> dict:
+        """Return information about the device."""
+        return {
+            "identifiers": {(DOMAIN, self.unique_id)},
+            "name": self.name,
+            "model": "OekoBoiler",
+            "manufacturer": "Oekoswiss Supply AG",
+        }
+
+    async def async_update(self, **kwargs) -> None:
+        self._state = "Test Modus"
+
+class OekoboilerWaterTempSensorEntiry(SensorEntity):
+    def __init__(
+        self,
+        hass: HomeAssistantType,
+        *args,
+        **kwargs,
+    ):
+        self._hass: HomeAssistantType = hass
+
+        self._state: int = None
+        super().__init__(*args, **kwargs)
+
+
+    @property
+    def name(self) -> str:
+        return f"Water Temperature"
+
+    @property
+    def unique_id(self) -> str:
+        return f"water_temp"
+
+    @property
+    def available(self) -> bool:
+        return True
+
+    @property
+    def state(self) -> int:
+        return self._state
+
+    @property
+    def device_class(self):
+        return DEVICE_CLASS_TEMPERATURE
+
+    @property
+    def state_class(self):
+        return "measurement"
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit the value is expressed in."""
+        return TEMP_CELSIUS
+
+
+    @property
+    def device_info(self) -> dict:
+        """Return information about the device."""
+        return {
+            "identifiers": {(DOMAIN, self.unique_id)},
+            "name": self.name,
+            "model": "OekoBoiler",
+            "manufacturer": "Oekoswiss Supply AG",
+        }
+
+    async def async_update(self, **kwargs) -> None:
+        self._state = "Test Modus"
+
+class OekoboilerSetTempSensorEntiry(SensorEntity):
+    def __init__(
+        self,
+        hass: HomeAssistantType,
+        *args,
+        **kwargs,
+    ):
+        self._hass: HomeAssistantType = hass
+
+        self._state: int = None
+        super().__init__(*args, **kwargs)
+
+
+    @property
+    def name(self) -> str:
+        return f"Set Temperature"
+
+    @property
+    def unique_id(self) -> str:
+        return f"set_temp"
+
+    @property
+    def available(self) -> bool:
+        return True
+
+    @property
+    def state(self) -> int:
+        return self._state
+
+    @property
+    def device_class(self):
+        return DEVICE_CLASS_TEMPERATURE
+
+    @property
+    def state_class(self):
+        return "measurement"
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit the value is expressed in."""
+        return TEMP_CELSIUS
 
     @property
     def device_info(self) -> dict:
