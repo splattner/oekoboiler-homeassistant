37cc06e5da7cf6a4408e437bac1ba202c4034863
Sebastain Plattner <sebastian.plattner@gmail.com>
2022-02-04 17:36:42 +0100

    fix drawing of boundry

--- custom_components/oekoboiler/oekoboiler.py
+++ custom_components/oekoboiler/oekoboiler.py
@@ -209,8 +209,8 @@ class Oekoboiler:
 
         opencv_image = cv.cvtColor(numpy.array(image), cv.COLOR_RGB2BGR)
 
-        for boundry in BOUNDRIES:
-            opencv_image = cv.rectangle(opencv_image,(boundry[0], boundry[1]),(boundry[2], boundry[3]),(0,255,0),1)
+        for key, value in self._boundries.items():
+            opencv_image = cv.rectangle(opencv_image,(value[0], value[1]),(value[2], value[3]),(0,255,0),1)
 
         _LOGGER.debug("Saving processed Image")
         self._image = Image.fromarray(cv.cvtColor(opencv_image, cv.COLOR_BGR2RGB) )
