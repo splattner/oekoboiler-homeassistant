76ff3b8a29837715f726e58551b6ab877f1413d3
Sebastian Plattner <plattner@puzzle.ch>
2022-03-30 20:45:23 +0200

    validation

--- custom_components/oekoboiler/config_flow.py
+++ custom_components/oekoboiler/config_flow.py
@@ -94,7 +94,7 @@ class OekoBoilerConfigFlow(ConfigFlow, domain=DOMAIN):
         data_schema = {
             vol.Required(CONF_CAMERA_ENTITY_ID, 
                          default=self.device_config[CONF_CAMERA_ENTITY_ID]
-                         ): cv.entity_domain(CAMERA_DOMAIN),
+                         ): cv.entity_id,
         }
 
         #vol.Any(cv.entity_id, cv.entity_domain(CAMERA_DOMAIN)
